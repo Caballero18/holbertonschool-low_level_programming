aced8c9c4f5b0134cb0cd9a3dd258a61b1149e11
Caballero18 <4617@holbertonschool.com>
2022-02-15 17:26:40 -0800

    1main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+*/
+void print_alphabet(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+
+	_putchar('\n');
+
+} 
