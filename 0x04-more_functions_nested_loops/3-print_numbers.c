a35722a8d93f465bf16debdf96de6e66c6430e0c
Caballero18 <4617@holbertonschool.com>
2022-02-17 13:14:52 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - use putchar
+ * @a: caracter random.
+ * return: alwas 0
+ */
+void print_numbers(void)
+{
+	for (a = '0'; a >= '9'; a++)
+	{
+	_putchar(a)
+	}
+	_putchar(\n)
+}
