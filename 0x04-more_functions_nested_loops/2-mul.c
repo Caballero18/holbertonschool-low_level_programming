38c4060b2a89129856a1938af1aced8e771ab723
Caballero18 <4617@holbertonschool.com>
2022-02-17 12:55:16 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ * @a: is a caracter
+ * @b: is a caracter.
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
