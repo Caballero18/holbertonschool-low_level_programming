248b0448718bd2282f0b05e5c6f552e8ed054db7
Caballero18 <4617@holbertonschool.com>
2022-02-17 11:46:21 -0800

    _isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,20 +2,16 @@
 #include <stdio.h>
 
 /**
- * _isupper - code.
- * @c: variable of function.
- *
- * Return: Always 0.
- */
-int _isupper(int c);
+* _isupper - function that check in uppercase.
+* @c: Variable that entry
+* Return: 1 
+*/
+int _isupper(int c)
 {
-	int c;
-
 	if (c >= 'A' && c <= 'Z')
 	{
-		return (1);
+	return (1);
 	}
-
 	else
 	{
 	return (0);
