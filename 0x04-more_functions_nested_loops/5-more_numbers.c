291c5cecdb825f8d092927828e5255b1ab2fe568
Caballero18 <4617@holbertonschool.com>
2022-02-17 17:54:47 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints digits from 0 to 14 ten times
+ */
+
+void more_numbers(void)
+
+{
+	int n;
+	char di;
+
+	for (n = 0; n < 10; ++n)
+
+	{
+
+		for (di = 0; di <= 14; ++di)
+
+		{
+			_putchar(di);
+
+		}
+
+		_putchar('\n');
+
+	}
+
+return;
+
+}
