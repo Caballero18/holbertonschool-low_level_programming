0917efe2cd1ec9ad8a512ba40cd754d84354f229
Caballero18 <4617@holbertonschool.com>
2022-03-03 17:40:50 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,21 +9,21 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (sz_sqrt_recursion(n, 0));
 }
 /**
- * actual_sqrt_recursion - recurses to find the natural
+ * sx_sqrt_recursion - recurses to find the natural
  * square root of a number
  * @n: number to calculate the sqaure root of
  * @i: iterator
  * Return: the resulting square root
  */
-int actual_sqrt_recursion(int n, int i)
+int sz_sqrt_recursion(int n, int i)
 {
 		if (i * i > n)
 			return (-1);
 
 			if (i * i == n)
 				return (i);
-			return (actual_sqrt_recursion(n, i + 1));
+			return (sz_sqrt_recursion(n, i + 1));
 }
