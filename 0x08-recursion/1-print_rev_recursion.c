1dedf568e4b5cfa1de6d96414c2f94b5401eaa58
Caballero18 <4617@holbertonschool.com>
2022-03-03 16:00:46 -0800

    rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - returns the impression
+ * Return: 0 always (sucess)
+ * @s: recursion
+ */
+void _print_rev_recursion(char *s)
+{
+	_print_rev_recursion(s + 1);
+	_putchar(*s)
+}
+
