6699ac6abc7c4b5e326cb025adf97811de63578e
Caballero18 <4617@holbertonschool.com>
2022-03-14 12:16:57 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE dog.h
+/**
+ * struct dog - define the struct of the dog.
+ * @*name: name of the dog.
+ * @age: age of the dog.
+ * @*owner: owner of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
