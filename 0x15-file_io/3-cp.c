648e6e48ac60b94767cc2213efd721677a8de234
Caballero18 <4617@holbertonschool.com>
2022-04-07 07:53:07 -0700

    change

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,30 +1,41 @@
 #include "main.h"
 /**
-* append_text_to_file - does what it say
-* @filename: the name of teh file
-* @text_content: The text
-* Return: int 1 ok -1 bad thing happen
-*/
-int append_text_to_file(const char *filename, char *text_content)
+ * main - Write a program that copies the
+ * content of a file to another file.
+ * @argc: arguments count
+ * @av: Arguments i.e filenames
+ * Return: Always 0
+ */
+
+int main(int argc, char *av[])
 {
-	int o, w, len = 0;
+	int fd_1, read_1, fd_2, write_2;
+	char buffer_1[1024];
 
-	if (filename == NULL)
-		return (-1);
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	if (text_content != NULL)
+	fd_1 = open(av[1], O_RDONLY);
+	if (fd_1 < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+
+	fd_2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
+
+	if (fd_2 < 0)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+
+	while ((read_1 = read(fd_1, buffer_1, 1024)) > 0)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		write_2 = write(fd_2, buffer_1, read_1);
+		if (write_2 < 0)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
-		return (-1);
-
-	close(o);
-
-	return (1);
+	if (read_1 < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	else if (close(fd_1) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_1), exit(100);
+	else if (close(fd_2) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_2), exit(100);
+	return (0);
 }
